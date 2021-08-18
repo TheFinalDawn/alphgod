bde6b1deb52dadd21b2913bd7aee616f04c05956
Resetium <ajwaite06@gmail.com>
2021-08-17 19:54:45 -0700

    0.8.0
    See Changelog. We goin public.

--- alph-gen/index.js
+++ alph-gen/index.js
@@ -1,30 +1,39 @@
 const fs = require("fs");
 process.chdir(__dirname);
-console.log(process.cwd());
-var used = fs.readFileSync("./used.txt", "utf8", (err, data) => {
-    if (err) console.error(err);
-});
+let used = JSON.parse(fs.readFileSync("./used.json", "utf8", (err, data) => {
+	if (err) console.error(err);
+}));
 const alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 
 function checkIfUsed(text) {
-    const sequence = used.split("\n");
-    for (segment in sequence) {
-        if (text == sequence[segment]) {
-            console.log("Generated an already existing sequence, regenerating...");
-            text = alph[Math.floor(Math.random() * 26)] + alph[Math.floor(Math.random() * 26)] + alph[Math.floor(Math.random() * 26)] + alph[Math.floor(Math.random() * 26)];
-            checkIfUsed(text);
-        }
-    }
-    used += `${text}\n`;
-    console.log(text);
-    return text
+	for (segment in used.used) {
+		if (text == used.used[segment][0]) {
+			console.log("Generated an already existing sequence, regenerating...");
+			text = alph[Math.floor(Math.random() * 26)] + alph[Math.floor(Math.random() * 26)] + alph[Math.floor(Math.random() * 26)] + alph[Math.floor(Math.random() * 26)];
+			checkIfUsed(text);
+		}
+	}
+	console.log(`NEW SEQUENCE GENERATED: ${text}`);
+	return text
 }
-exports.generate = function() {
-    var out = alph[Math.floor(Math.random() * 26)] + alph[Math.floor(Math.random() * 26)] + alph[Math.floor(Math.random() * 26)] + alph[Math.floor(Math.random() * 26)];
-    var confirm = checkIfUsed(out);
-    return confirm;
+exports.generate = function(user = "test") {
+	let out = alph[Math.floor(Math.random() * 26)] + alph[Math.floor(Math.random() * 26)] + alph[Math.floor(Math.random() * 26)] + alph[Math.floor(Math.random() * 26)];
+	let egg = "";
+	if (out[0] == out[1] && out[1] == out[2] && out[2] == out[3]) {
+		egg = "same";
+	} else if (out[0] == out[3] && out[1] == out[2]) {
+		egg = "symm";
+	}
+	let confirm = checkIfUsed(out);
+	let next = {
+		sequence: confirm,
+		username: user
+	}
+	let newIndex = Object.keys(used.used).length;
+	used.used[newIndex] = next;
+	console.log(used);
+	used = JSON.stringify(used);
+	fs.writeFileSync("./used.json", used);
+	return confirm, egg;
 }
-var out = alph[Math.floor(Math.random() * 26)] + alph[Math.floor(Math.random() * 26)] + alph[Math.floor(Math.random() * 26)] + alph[Math.floor(Math.random() * 26)];
-checkIfUsed(out);
-fs.writeFileSync("./used.txt", used);
--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 const Snoowrap = require('snoowrap');
 const { CommentStream } = require('snoostorm');
 const BOT_START = Date.now() / 1000;
-const sequencer = require("../alph-gen/index.js");
-const masterpost_id = "t3_hy2g45";
+const sequencer = require("./alph-gen/index.js");
+const config = require("./config.json"); // this is important
+const masterpost_id = config.masterpostID
 
-const client = new Snoowrap({
-    userAgent: "alph",
-    clientId: "NpwjUrLIdQzGVA",
-    clientSecret: "36wVM0b9qWHoWkmH2gJlwX252pA",
-    username: "alphgod",
-    password: "NMmNGqj#xp\"28ng)_vF/"
-});
+const client = new Snoowrap(config.nordvpnAdvertisementHere); // turn on phone
 
-const comments = new CommentStream(client, {
+const comments = new CommentStream(client, { // open reddit
     subreddit: 'alph',
-    limit: 10,
+    limit: 100,
     pollTime: 10000
 });
 
 comments.on('item', (item) => {
     console.log(item);
     if (item.created_utc < BOT_START) return;
-    if (item.link_id !== masterpost_id || item.link_id !== item.parent_id) return;
+    if (item.link_id !== masterpost_id || item.link_id !== item.parent_id) return; // check masterpost
     if (item.author_flair_text) {
         item.reply("**Greedy individuals will not be tolerated. I shall only give you one sequence, no more than that. Should you do this again, consequences will follow.**");
         item.report({ reason: "Attempted to acquire multiple sequences." });
-        item.remove();
+        item.remove(); // :angry:
     } else {
-        let theirSequence = sequencer.generate();
+        let theirSequence, egg = sequencer.generate(item.author.username);
+        if (egg == "same") {
+            item.reply(`Most unusual. You have been assigned the sequence ${theirSequence}. You're clearly destined to be a leader, should you choose to accept this role or not.`); // :scream:
+        } else if (egg == "symm") {
+            item.reply(`Very interesting. You have been assigned the sequence ${theirSequence}. May you go on to become one of the most powerful sequenced in the land.`) // :open_mouth:
+            // this will likely be the "special sequence" message when I figure out modular pattern recognition.
+        } else {
+            item.reply(`You have been assigned the sequence ${theirSequence}. May it be known forever in the pages of history.`); // :slight_smile:
+		}
         item.author.assignFlair({ subredditName: "alph", text: theirSequence });
-        item.reply(`You have been assigned the sequence ${theirSequence}. May it be known forever in the pages of history.`);
+        
     }
 
 });
